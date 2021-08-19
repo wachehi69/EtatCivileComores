2f1e96db0d249f8000a485e191163041f70a06e6
wachehi69 <64372485+wachehi69@users.noreply.github.com>
2021-08-19 17:10:21 +0200

    Update GestionMairieComoresApplicationTests.java

--- GestionMairieComores/src/test/java/org/mairie/comores/GestionMairieComoresApplicationTests.java
+++ GestionMairieComores/src/test/java/org/mairie/comores/GestionMairieComoresApplicationTests.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit5.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
