8d6119682155bd47737ac359efebc7c9bcf0b407
wachehi69 <wachehi6905@gmail.com>
2021-08-25 21:29:12 +0200

    correction1

--- GestionMairieComores/src/test/java/org/mairie/comores/GestionMairieComoresApplicationTests.java
+++ GestionMairieComores/src/test/java/org/mairie/comores/GestionMairieComoresApplicationTests.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class GestionMairieComoresApplicationTests {
 
-	// test
+	// test1
 @Test
 	public void contextLoads() {
 	}
