af343b238233677e68cd2bd53a24842ecd879571
wachehi69 <wachehi6905@gmail.com>
2021-08-31 21:40:03 +0200

    test

--- GestionMairieComores/src/test/java/org/mairie/comores/GestionMairieComoresApplicationTests.java
+++ GestionMairieComores/src/test/java/org/mairie/comores/GestionMairieComoresApplicationTests.java
@@ -1,6 +1,6 @@
 package org.mairie.comores;
 
-import static org.assertj.core.api.Assertions.assertThat;
+//import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
